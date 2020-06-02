89849df6ff5ec2272e1a7152c046548821093628
Wanghaoyi123 <45254018+Wanghaoyi123@users.noreply.github.com>
2020-06-02 17:42:57 +0800

    Update deletefood.php

--- deletefood.php
+++ deletefood.php
@@ -1,20 +1,6 @@
-<?php
+<?php 
 
-// 1. Open database connection
-$dbhost = "localhost";
-$dbuser = "52721f5c61c4";
-$dbpass = "2456ee08541f2b9e";
-$dbname = "l5k2d";
-
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-// Test if connection is ok
-if (mysqli_connect_errno()) {
-    die("Database connection failed: " .
-        mysqli_connect_error() .
-        " (" . mysqli_connect_errno() . ")" 
-    );
-}
+require_once('db.php');
 
 $deleteid = $_GET['id'];
 
@@ -32,7 +18,7 @@ if (!$result) {
     die("query is wrong");
 }
 
-Echo "cases deleted";
+Echo "food deleted";
 
 // 5. close db connection
 mysqli_close($connection);
